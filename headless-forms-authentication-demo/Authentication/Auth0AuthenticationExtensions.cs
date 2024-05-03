f3b0da76201ff571204816533966e428ded41710
Andrew McKaskill <andrew.mckaskill@greatstate.co>
2024-05-03 12:50:43 +0100

    Add Auth0 authentication extensions

---
+++ headless-forms-authentication-demo/Authentication/Auth0AuthenticationExtensions.cs
@@ -0,0 +1,61 @@
+
+using Auth0.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OAuth;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Extensions.Options;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Web.Common.Security;
+
+namespace headless_forms_authentication_demo.Authentication;
+
+public static class Auth0AuthenticationExtensions
+{
+    private const string Scheme = "Auth0";
+
+    public static IUmbracoBuilder AddAuth0MemberAuthentication(this IUmbracoBuilder builder)
+    {
+        builder.Services.ConfigureOptions<Auth0MemberExternalLoginProviderOptions>();
+
+        builder.AddMemberExternalLogins(logins =>
+        {
+            logins.AddMemberLogin(
+                membersAuthenticationBuilder =>
+                {
+                    membersAuthenticationBuilder.AddAuth0WebAppAuthentication(
+                        membersAuthenticationBuilder.SchemeForMembers(Scheme),
+                        options =>
+                        {
+                            // add your client ID and secret here
+                            options.Domain = builder.Config["Auth0:Domain"];
+                            options.ClientId = builder.Config["Auth0:ClientId"];
+                            options.Scope = "openid profile email";
+                            options.SkipCookieMiddleware = true;
+                            
+                        });
+                });
+        });
+
+        return builder;
+    }
+
+    private class Auth0MemberExternalLoginProviderOptions : IConfigureNamedOptions<MemberExternalLoginProviderOptions>
+    {
+        public void Configure(string? name, MemberExternalLoginProviderOptions options)
+        {
+            if (name is not $"{Constants.Security.MemberExternalAuthenticationTypePrefix}{Scheme}")
+            {
+                return;
+            }
+
+            Configure(options);
+        }
+
+        public void Configure(MemberExternalLoginProviderOptions options)
+            => options.AutoLinkOptions = new MemberExternalSignInAutoLinkOptions(
+                autoLinkExternalAccount: true,
+                defaultCulture: null,
+                defaultIsApproved: true,
+                defaultMemberTypeAlias: Constants.Security.DefaultMemberTypeAlias);
+    }
+}
+
---
+++ headless-forms-authentication-demo/Composers/AuthenticationComposer.cs
@@ -0,0 +1,12 @@
+using headless_forms_authentication_demo.Authentication;
+using Umbraco.Cms.Core.Composing;
+
+namespace headless_forms_authentication_demo.Composers;
+
+public class AuthenticationComposer : IComposer
+{
+    public void Compose(IUmbracoBuilder builder)
+    {
+        builder.AddAuth0MemberAuthentication();
+    }
+}
